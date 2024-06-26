91c69a090b57bdcdf4626899de65ab61ebdc90f4
Цыганенко <lolkinalex@yandex.ru>
2024-06-26 17:03:47 +0300

    Доделал сайт.

--- scripts.js
+++ scripts.js
@@ -1,36 +1,78 @@
 let screenshotPanel = document.getElementById("screenshotPanel");
 let leftBtn = document.getElementById("leftBtn");
 let rightBtn = document.getElementById("rightBtn");
-let maxCount = 2
-let rightCount = 0
-let leftCount = 0
+let images = document.querySelectorAll(".gallery img");
+let imageWidth = 1020; 
+let numberOfImages = images.length;
 
-rightBtn.addEventListener('click', ()=> {
-    if (rightCount == maxCount){
-        screenshotPanel.style.scrollBehavior = "smooth";
-        screenshotPanel.scrollLeft = 0;
-        rightCount = 0;
-    } else {
-        screenshotPanel.style.scrollBehavior = "smooth";
-        screenshotPanel.scrollLeft += 1020;
-        ++rightCount;
+function updateScrollPosition(newIndex) {
+    let newScrollPosition = newIndex * imageWidth;
+    screenshotPanel.scrollLeft = newScrollPosition;
+}
+
+function getCurrentIndex() {
+    return Math.round(screenshotPanel.scrollLeft / imageWidth);
+}
+
+rightBtn.addEventListener('click', () => {
+    let currentIndex = getCurrentIndex();
+    let newIndex = currentIndex + 1;
+    if (newIndex >= numberOfImages) {
+        newIndex = 0;
     }
+    updateScrollPosition(newIndex);
 });
 
-leftBtn.addEventListener('click', ()=> {
-    if (leftCount === maxCount){
-        screenshotPanel.style.scrollBehavior = "smooth";
-        screenshotPanel.scrollLeft = 0;
-        leftCount = 0;
-    } else {
-        if (screenshotPanel.scrollLeft == 0) {
-            screenshotPanel.style.scrollBehavior = "smooth";
-            screenshotPanel.scrollLeft += 2040;
-            leftCount = 0;
+leftBtn.addEventListener('click', () => {
+    let currentIndex = getCurrentIndex();
+    let newIndex = currentIndex - 1;
+    if (newIndex < 0) {
+        newIndex = 3;
+    }
+    updateScrollPosition(newIndex);
+});
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    let robot = document.getElementById('robot');
+    let moveDistance = 2;
+    let position = 0;
+    let moveRight = true;
+
+    function moveRobot() {
+        let parentWidth = robot.parentElement.offsetWidth - 28;
+        let robotWidth = robot.offsetWidth;
+
+        if (moveRight) {
+            position += moveDistance;
+            if (position >= (parentWidth - robotWidth)) {
+                moveRight = false;
+                flip();
+            }
         } else {
-            screenshotPanel.style.scrollBehavior = "smooth";
-            screenshotPanel.scrollLeft -= 1020;
-            ++leftCount;
+            position -= moveDistance;
+            if (position <= 0) {
+                moveRight = true;
+                flip();
+            }
         }
+
+        robot.style.left = position + 'px';
     }
+
+    robot.addEventListener('click', () => {
+        moveDistance += 0.2; 
+    
+        console.log(moveDistance)
+    });
+
+    setInterval(moveRobot, 5);
 });
+
+function flip() {
+    if (robot.style.transform === 'scaleX(1)') {
+        robot.style.transform = 'scaleX(-1)'; 
+    } else {
+        robot.style.transform = 'scaleX(1)'; 
+    }
+}
